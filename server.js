96df67eb4ccf0f125759bcfca3c58eab58c0dafa
Anastasia Perez <Stasiperez85@gmail.com>
2022-02-24 16:44:36 -0700

    Add Heroku

---
+++ server.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+const { notes } = require('./Develop/db/db.json');
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
